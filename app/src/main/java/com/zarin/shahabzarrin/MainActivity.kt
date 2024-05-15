6e7e98f31d23ab4b2a0af5df9a870000f47f20c0
aliyousefpoor <aliyousefpoor47@gmail.com>
2024-05-15 11:38:36 +0330

    add adapter in main activity

--- app/src/main/java/com/zarin/shahabzarrin/MainActivity.kt
+++ app/src/main/java/com/zarin/shahabzarrin/MainActivity.kt
@@ -2,10 +2,24 @@ package com.zarin.shahabzarrin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private val adapter: MediaAdapter by lazy {
+        MediaAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.mediaList)
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        recyclerView.layoutManager = GridLayoutManager(baseContext, 4)
+        recyclerView.adapter = adapter
     }
 }
