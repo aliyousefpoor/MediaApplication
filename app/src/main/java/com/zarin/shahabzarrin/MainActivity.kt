4626bee5c6cf3bb4b211ac813f770ce15f170e60
aliyousefpoor <aliyousefpoor47@gmail.com>
2024-05-15 12:25:45 +0330

    load media

--- app/src/main/java/com/zarin/shahabzarrin/MainActivity.kt
+++ app/src/main/java/com/zarin/shahabzarrin/MainActivity.kt
@@ -1,25 +1,101 @@
 package com.zarin.shahabzarrin
 
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private val adapter: MediaAdapter by lazy {
-        MediaAdapter()
-    }
+    private lateinit var adapter: MediaAdapter
+    private val mediaList = mutableListOf<MediaItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.mediaList)
+//        checkStoragePermission()
         setupAdapter()
     }
+
+    private fun checkStoragePermission() {
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            loadMedia()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadMedia()
+        }
+    }
+
+    private fun loadMedia() {
+        val imageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val videoUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+
+        val projection = arrayOf(MediaStore.MediaColumns._ID, MediaStore.MediaColumns.MIME_TYPE)
+        val imagesCursor = contentResolver.query(imageUri, projection, null, null, null)
+        val videosCursor = contentResolver.query(videoUri, projection, null, null, null)
+
+        imagesCursor?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID)
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val uri = Uri.withAppendedPath(imageUri, id.toString())
+                mediaList.add(MediaItem(uri, MediaType.IMAGE))
+            }
+        }
+
+        videosCursor?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns._ID)
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val uri = Uri.withAppendedPath(videoUri, id.toString())
+                mediaList.add(MediaItem(uri, MediaType.VIDEO))
+            }
+        }
+
+        adapter.notifyDataSetChanged()
+    }
+
 
     private fun setupAdapter() {
         recyclerView.layoutManager = GridLayoutManager(baseContext, 4)
         recyclerView.adapter = adapter
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+//                loadMedia()
+            } else {
+                Toast.makeText(this, "اجازه دسترسی را نداده اید.", Toast.LENGTH_SHORT).show()
+            }
+        }
 }
